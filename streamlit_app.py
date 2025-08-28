1c050900ae5a5971404079acccc3b6417c5cbf65
Adrian Fernandez Cuevas <156624665+adfecu@users.noreply.github.com>
2025-08-28 15:27:44 +0000

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,21 @@ import streamlit as st
 
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
+<style>
 #GithubIcon {
   visibility: hidden;
 }
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title("Jan Carlos Placencio")
 st.write(
