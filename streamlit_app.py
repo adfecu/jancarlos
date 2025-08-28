80ca8ce74a3f413cc9993f02144105a4897f10cf
Adrian Fernandez Cuevas <156624665+adfecu@users.noreply.github.com>
2025-08-28 15:25:20 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.title("Jan Carlos Placencio")
 st.write(
     "Bienvenidos a mi página web"
