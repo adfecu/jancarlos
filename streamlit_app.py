22d1628ab6d16fe3fd90be863d2d7dd420969a78
Adrian Fernandez Cuevas <156624665+adfecu@users.noreply.github.com>
2025-08-28 14:38:56 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Jan Carlos Placencio")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Bienvenidos a mi página web"
 )
